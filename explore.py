00b556a9f7fc16968cef0697cb31f1fee0d70bf5
Chloe Whitaker <chloerwhitaker@gmail.com>
2022-02-10 23:04:59 -0600

    explore functions

---
+++ explore.py
@@ -0,0 +1,160 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# ## Helper File Containing the Explore Functions
+
+# ### Imports
+
+# In[1]:
+
+
+import pandas as pd
+import numpy as np
+
+# Scraping
+import requests
+from bs4 import BeautifulSoup
+
+import nltk
+from nltk.tokenize.toktok import ToktokTokenizer
+from nltk.corpus import stopwords
+import nltk.sentiment
+
+from wordcloud import WordCloud
+from PIL import Image
+
+# Regex
+import re
+
+# Time
+from time import strftime
+
+import unicodedata
+import json
+from pprint import pprint
+
+# Visualizations
+import matplotlib.pyplot as plt
+import seaborn as sns
+
+import wrangle 
+
+# Turn off pink boxes for demo
+import warnings
+warnings.filterwarnings("ignore")
+
+# pd.set_option('display.max_colwidth', -1)
+
+
+# In[2]:
+
+
+df = pd.read_json('repo_readmes.json')
+
+
+# In[3]:
+
+
+# Save the names of the top 5 programming languages, and change the rest to 'Other'
+df['language'] = df.language.apply(wrangle.common_language)
+
+
+# In[4]:
+
+
+df = wrangle.brian_quick_clean(df)
+
+
+# In[5]:
+
+
+train, validate, test, X_train, y_train, X_validate, y_validate, X_test, y_test = wrangle.split_repos(df)
+
+
+# In[6]:
+
+
+# combine all contents in single string by language
+
+javascript_words = (' '.join(train[train.language == 'JavaScript'].lemmatized))
+c_plus_plus__words = (' '.join(train[train.language == 'C++'].lemmatized))
+c_words = (' '.join(train[train.language == 'C'].lemmatized))
+python_words = (' '.join(train[train.language == 'Python'].lemmatized))
+php_words = (' '.join(train[train.language == 'PHP'].lemmatized))
+java_words = (' '.join(train[train.language == 'Java'].lemmatized))
+other_words = (' '.join(train[train.language == 'Other'].lemmatized))
+all_words = (' '.join(train.lemmatized))
+
+
+# In[7]:
+
+
+# remove_stopwords
+
+javascript_words = wrangle.remove_stopwords(str(javascript_words))
+c_plus_plus__words = wrangle.remove_stopwords(str(c_plus_plus__words))
+c_words = wrangle.remove_stopwords(str(c_words))
+python_words = wrangle.remove_stopwords(str(python_words))
+java_words = wrangle.remove_stopwords(str(java_words))
+php_words = wrangle.remove_stopwords(str(php_words))
+other_words = wrangle.remove_stopwords(str(other_words))
+all_words = wrangle.remove_stopwords(str(all_words))
+
+
+# In[8]:
+
+
+# represent contents as word frequencies
+
+javascript_freq = pd.Series(javascript_words.split()).value_counts()
+c_plus_plus_freq = pd.Series(c_plus_plus__words.split()).value_counts()
+c_freq = pd.Series(c_words.split()).value_counts()
+python_freq = pd.Series(python_words.split()).value_counts()
+java_freq = pd.Series(java_words.split()).value_counts()
+php_freq = pd.Series(php_words.split()).value_counts()
+other_freq = pd.Series(other_words.split()).value_counts()
+all_freq = pd.Series(all_words.split()).value_counts()
+
+
+# In[9]:
+
+
+# concat all frequencies together into a dataframe
+
+word_counts = pd.concat([javascript_freq, c_plus_plus_freq, c_freq, python_freq, java_freq, php_freq, other_freq, all_freq], axis=1).fillna(0).astype(int)
+word_counts.columns = ['javascript', 'c_plus_plus', 'c', 'python', 'java', 'php', 'other', 'all']
+# word_counts.head()
+
+
+# In[13]:
+
+
+def github_logo():
+    ''' 
+    This function: 
+    returns a github logo word cloud'''
+    plt.figure(figsize=(16,9))
+    mask = np.array(Image.open("octocat_logo.png"))
+    wc = WordCloud(background_color="black", width=800, height=400, contour_width=1, contour_color='white',mask=mask)
+    # generate word cloud
+    wc.generate_from_frequencies(word_counts['all'])
+
+    # show
+    plt.imshow(wc, interpolation="bilinear")
+    plt.axis("off")
+    plt.show()
+
+    return 
+
+
+# In[15]:
+
+
+# github_logo()
+
+
+# In[ ]:
+
+
+
+
